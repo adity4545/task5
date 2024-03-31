da776a9f7303894ceeaa9170bf3f17eea35a7c89
adity4545 <165558221+adity4545@users.noreply.github.com>
2024-03-31 17:03:12 +0530

    Update script.js
    change image path

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ async function checkWeather(city){
 
     switch(weather_data.weather[0].main){
         case 'Clouds':
-            weather_img.src = "/assets/cloud.png";
+            weather_img.src = "/cloud.png";
             break;
         case 'Clear':
             weather_img.src = "/assets/clear.png";
